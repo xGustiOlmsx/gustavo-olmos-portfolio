484745c11de4ceea6a788866a05e66a3d5ce0dc6
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-09-20 22:46:03 -0300

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import Home from "./componentes/Home.js";
+import About from "./componentes/About";
+import Portfolio from "./componentes/Portfolio";
+import Contactos from "./componentes/Contactos";
+import Servicios from "./componentes/Servicios";
+import Nav from "./Nav";
+import Error404 from "./componentes/Error404";
+import Descripción from "./componentes/Descripción.js";
+import Footer from "./Footer";
 import './App.css';
+import {BrowserRouter,Route,Routes,Navigate} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav/>
+      <Descripción/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Portfolio" element={<Portfolio/>}/>
+        <Route path="/Servicios" element={<Servicios/>}/>
+        <Route path="/Contactos" element={<Contactos/>}/>
+        <Route path="/Home" element={<Navigate to="/"/>}/>
+        <Route path="*" element={<Error404/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
