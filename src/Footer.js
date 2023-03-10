dc8c2421dab584e4b55b1160ef93f0f3a8e273db
xGustiOlmsx <gustavoolmos002@gmail.com>
2023-03-10 05:08:03 -0300

    linkeadas las redes sociales en el footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className='footer'>
       <div className='icons-redes'>
-        <a className='icon-facebook icon'><i class="fa-brands fa-facebook-f"></i></a>
-        <a className='icon-linkedin icon'><i class="fa-brands fa-linkedin-in"></i></a>
-        <a className='icon-github icon'><i class="fa-brands fa-github"></i></a>
-        <a className='icon-instagram icon'><i class="fa-brands fa-instagram"></i></a>
+        <a className='icon-facebook icon' href='https://www.facebook.com/gustavo.olmos.528316'><i class="fa-brands fa-facebook-f"></i></a>
+        <a className='icon-linkedin icon' href='https://www.linkedin.com/in/gustavo-olmos-530b13215/'><i class="fa-brands fa-linkedin-in"></i></a>
+        <a className='icon-github icon' href='https://github.com/xGustiOlmsx'><i class="fa-brands fa-github"></i></a>
+        <a className='icon-instagram icon' href='https://www.instagram.com/gustavo__olmos/'><i class="fa-brands fa-instagram"></i></a>
       </div>
       <div className='copy'>
         Copyright <i class="fa-regular fa-copyright"></i> 2022 Gustavo Olmos | Diseñador y desarrollador web independiente San Justo - Santa Fe
